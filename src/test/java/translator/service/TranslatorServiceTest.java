f5e71aadd49c8bcb84dfd17caa37f36153579739
John David <david9352@hotmail.com>
2016-10-28 12:00:42 +0200

    Change to the Test

--- src/test/java/translator/service/TranslatorServiceTest.java
+++ src/test/java/translator/service/TranslatorServiceTest.java
@@ -11,17 +11,46 @@ import translator.domain.TranslatedText;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class TranslatorServiceTest {
 
+private	ArrayList<String> LanguagesList = new ArrayList<>();	
+private	ArrayList<String> TextList = new ArrayList<>();
+
+
     @Autowired
     TranslatorService translatorService;
     @Test
     public void translateTest() throws Exception {
-        TranslatedText translatedText = translatorService.translate("en", "es", "This is a test of translation service");
-        assertEquals("Esto es una prueba de servicio de traducción",translatedText.getTranslation());
+    	
+    LanguagesList.add("en");
+    LanguagesList.add("es");
+    LanguagesList.add("fr");
+    LanguagesList.add("de");
+    LanguagesList.add("pt");
+    LanguagesList.add("it");
+    LanguagesList.add("ca");
+    LanguagesList.add("ja");
+    
+    TextList.add("Test of translation service for various languages");
+    TextList.add("Prueba de servicio de traducción para varios idiomas");
+    TextList.add("Essai de traduction en différentes langues");
+    TextList.add("Test von übersetzungs service für diverse Sprachen");
+    TextList.add("Teste de serviço de tradução para vários idiomas");
+    TextList.add("Test del servizio di traduzione in diverse lingue");
+    TextList.add("Prova de servei de traducció per a diversos idiomes");
+    TextList.add("試験の翻訳サービスのための様々な言語");
+    	
+    	for (int i = 1; i < TextList.size(); i++) {
+    		TranslatedText translatedText = translatorService.translate(LanguagesList.get(0),
+				LanguagesList.get(i), TextList.get(0));
+    		assertEquals(TextList.get(i),translatedText.getTranslation());
+    	}
+        
     }
 
 }
